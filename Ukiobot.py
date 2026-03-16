36c1465661387827c15a585f670afcc657be5486
ZhAkzhan <119696444+ZhAkzhan@users.noreply.github.com>
2026-03-16 23:28:21 +0500

    Replace hardcoded token with environment variable

--- Ukiobot.py
+++ Ukiobot.py
@@ -12,7 +12,7 @@ from telegram.ext import (
     filters
 )
 
-TOKEN = "8711612037:AAFSOm_3NZBniSEqzhVuv-ngFYZpItl0W-Q"
+TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
 # ===== PATH TO DATABASE =====
 BASE_DIR = os.path.expanduser("~/Desktop")
